ca4e90acf7a8d84b5b769397b68f56fc45937854
Rabebar <rabe.bar.a74@gmail.com>
2026-03-23 16:46:39 +0200

    إضافة روت لـ work-manifest

--- app.py
+++ app.py
@@ -774,6 +774,12 @@ def save_upload(file, prefix='img'):
 @app.route('/manifest.json')
 def manifest():
     return app.send_static_file('manifest.json')
+@app.route('/work-manifest.json')
+def work_manifest():
+    from flask import Response
+    with open('static/work-manifest.json') as f:
+        content = f.read()
+    return Response(content, mimetype='application/manifest+json')
 
 @app.route('/service-worker.js')
 def service_worker():
